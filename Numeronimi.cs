97f3ea2ff0967d4664bc96c36819d005e0aded90
ricbianchi <info@riccardobianchi.net>
2023-01-09 10:10:20 +0100

    Modifica signature metodo

--- Numeronimi.cs
+++ Numeronimi.cs
@@ -2,11 +2,16 @@ namespace Numeronimi
 {
     public static class Numeronimo
     {
-        public static string Numeronymize(string s)
+        public static string Numeronymize(string s, int mode)
         {
+            //Se mode == 0 tutte minuscole, salvo i casi particolari indicati
+            //Se mode == 1 Le iniziali del numeronimo dovranno essere maiuscole, salvo i casi
+            //particolari indicati
             string res = "";
 
             return res;
         }
+
+
     }
 }
